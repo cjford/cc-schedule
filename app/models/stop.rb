c702134bcdd5c5ab0de6ec8895ab20da82020127
Connor Ford <cjford@colgate.edu>
2012-11-26 20:42:13 -0500

    Added functions for upcoming stops

--- app/models/stop.rb
+++ app/models/stop.rb
@@ -1,13 +1,32 @@
 class Stop < ActiveRecord::Base
   belongs_to :line
   belongs_to :location
-  attr_accessible :line_id, :location_id, :days, :time
+  attr_accessible :line_id, :location_id, :hour, :minute, 
+                  :monday, :tuesday, :wednesday, :thursday, :friday, :saturday, :sunday 
 
-  def self.get_line_stops(id)
+  def self.line_stops(id)
     Stop.where(:line_id => id)
   end
 
-  def self.get_location_stops(id)
+  def self.location_stops(id)
     Stop.where(:location_id => id)
+  end
+
+  def self.upcoming_stops_any(num_stops)
+    Stop.find(:all, {
+                :conditions => ["hour >= ? AND minute >= ? AND #{Time.now.strftime("%A")} = 't'", 
+                  Time.now.hour, Time.now.min], 
+                :order => "hour, minute", 
+                :limit => num_stops
+              })
+  end
+
+  def self.upcoming_stops(num_stops, location)
+    Stop.find(:all, {
+                :conditions => ["hour >= ? AND minute >= ? AND location = ? AND #{Time.now.strftime("%A")} = 't'", 
+                  Time.now.hour, Time.now.min, location], 
+                :order => "hour, minute", 
+                :limit => num_stops
+              })
   end
 end
