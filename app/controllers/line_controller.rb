3c985eb80f7cc396c708e0f1c0f95b9171e6404e
Connor Ford <cjford@colgate.edu>
2012-11-26 19:57:20 -0500

    Removed incorrectly named controllers and default views

--- app/controllers/line_controller.rb
+++
@@ -1,9 +0,0 @@
-class LineController < ApplicationController
-  def index
-    @lines = Line.all
-  end
-
-  def view
-    @stops = Stop.get_line_stops(params[:id])
-  end
-end
--- app/controllers/location_controller.rb
+++
@@ -1,9 +0,0 @@
-class LocationController < ApplicationController
-  def index
-    @locations = Location.all
-  end
-
-  def view
-    @stops = Stop.get_location_stops(params[:id])
-  end
-end
