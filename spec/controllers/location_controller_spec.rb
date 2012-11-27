e67468bac710464bace20e4527faf8b14faa230d
Connor Ford <cjford@colgate.edu>
2012-11-26 21:54:19 -0500

    Updated some action names

--- spec/controllers/location_controller_spec.rb
+++ spec/controllers/location_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe LocationController do
+describe LocationsController do
   describe "GET 'index'" do
 
     it "returns http success" do
@@ -20,29 +20,29 @@ describe LocationController do
   end
 
 
-  describe "GET 'view'" do
+  describe "GET 'show'" do
 
     it "returns http success" do
-      get 'view', :id => 1
+      get 'show', :id => 1
       response.should be_success
     end
 
-    it "renders the 'view' view" do
-      get 'view', :id => 1
-      response.should render_template(:view)
+    it "renders the 'show' view" do
+      get 'show', :id => 1
+      response.should render_template(:show)
     end
 
     it "calls the Stop model method to get stops for the specified location" do
-      Stop.stub :get_location_stops
-      Stop.should_receive :get_location_stops
-      get 'view', :id => 1
+      Stop.stub :location_stops
+      Stop.should_receive :location_stops
+      get 'show', :id => 1
     end
 
     it "makes location information available in the view" do
       fake_results = [mock('Stop1'), mock('Stop2')]
-      Stop.stub :get_location_stops
-      Stop.should_receive(:get_location_stops).and_return(fake_results)
-      get 'view', :id => 1
+      Stop.stub :location_stops
+      Stop.should_receive(:location_stops).and_return(fake_results)
+      get 'show', :id => 1
       assigns(:stops).should == fake_results
     end
 
