f83965aa50cdea9158ba7f9efb03a01fca553aa0
Connor Ford <cjford@colgate.edu>
2012-12-02 22:05:47 -0500

    Added filter form processing and partial refresh action

--- app/controllers/stops_controller.rb
+++ app/controllers/stops_controller.rb
@@ -1,6 +1,28 @@
 class StopsController < ApplicationController
   def index
-    @locations = Location.all
-    @stops = Stop.upcoming_stops(10, nil)
+    if request.post?
+      cookies[:line_filter] = ''
+      Line.all.each do |line|
+        if !params[line.name].nil?
+          cookies[:line_filter] += line.id.to_s 
+        end
+      end
+      redirect_to request.referer
+    end
+
+    @locations = Location.order("name")
+    @lines = Line.all
+    @stops = Stop.upcoming_stops(10, nil, cookies[:line_filter].split(''))
+  end
+
+  def refresh
+    if params[:location] == 'All'
+      location = nil
+    else
+      location = Location.where(:name => params[:location]).first.id
+    end
+
+    @stops = Stop.upcoming_stops(10, location, cookies[:line_filter].split(''))
+    render :partial => "upcoming_stops"
   end
 end
