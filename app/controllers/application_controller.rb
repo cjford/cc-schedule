d460c8e506539973b8e0188538be7546601f2800
Connor Ford <cjford@colgate.edu>
2012-12-02 22:06:09 -0500

    Added before_filter method to set cookies

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :set_filter
+  
+  def set_filter
+    if cookies[:line_filter].nil?
+      cookies[:line_filter] = ''
+      Line.all.each do |line|
+        cookies[:line_filter] += line.id.to_s
+      end
+    end
+  end
+
 end
