dbd9c7e0757dae95a24b3da1f56a071e22afbb36
Connor Ford <cjford@colgate.edu>
2012-12-02 21:55:29 -0500

    Added assets

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+
+  $('#locations_toggle').click(function() {
+    var src = ($('#locations_toggle').attr('src') === 'dropdown_arrow.gif' ? 'dropup_arrow.gif' : 'dropdown_arrow.gif');
+    $('#locations_toggle').attr('src', src);
+    $('#locations_dropdown').slideToggle();
+
+    if($('#lines_dropdown').css('display') != 'none') {
+      $('#lines_dropdown').slideUp(100);
+      $('#lines_toggle').attr('src', 'dropdown_arrow.gif');
+    }
+  });
+
+  $('#lines_toggle').click(function() {
+    var src = ($('#lines_toggle').attr('src') === 'dropdown_arrow.gif' ? 'dropup_arrow.gif' : 'dropdown_arrow.gif');
+    $('#lines_toggle').attr('src', src);
+    $('#lines_dropdown').slideToggle();
+
+    if($('#locations_dropdown').css('display') != 'none') {
+      $('#locations_dropdown').slideUp(100);
+      $('#locations_toggle').attr('src', 'dropdown_arrow.gif');
+    }
+  });
+
+  $('#locations_select').change(function() {
+    $('#upcoming_stops').load('/refresh/'+this.value)
+  });
+
+  $('#refresh').click(function() {
+    $('#upcoming_stops').load('/refresh/'+$('#Location').attr('value'));
+  });
+
+});
