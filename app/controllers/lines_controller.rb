ab797f19caae8da3cb81db722127d84f65f7eda5
Connor Ford <cjford@colgate.edu>
2012-12-07 03:25:21 -0500

    Downcased some params in attempt to fix postgres bug on heroku

--- app/controllers/lines_controller.rb
+++ app/controllers/lines_controller.rb
@@ -4,13 +4,13 @@ class LinesController < ApplicationController
   end
 
   def show
-    @stops = Stop.where("#{Time.now.strftime('%A')}" => 't').line(params[:id])
+    @stops = Stop.where("#{Time.now.strftime('%A').downcase}" => 't').line(params[:id])
     @line_name = Line.find(params[:id]).name
     @line_id = params[:id]
   end
 
   def refresh_line
-    @stops = Stop.line(params[:line_id]).where(params[:day] => 't')
+    @stops = Stop.line(params[:line_id]).where(params[:day].downcase => 't')
     render :partial => 'line_stops'
   end
 end
--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -4,13 +4,13 @@ class LocationsController < ApplicationController
   end
 
   def show
-    @stops = Stop.where("#{Time.now.strftime('%A')}" => 't').line(cookies[:line_filter].split('')).location(params[:id])
+    @stops = Stop.where("#{Time.now.strftime('%A').downcase}" => 't').line(cookies[:line_filter].split('')).location(params[:id])
     @location_name = Location.find(params[:id]).name
     @location_id = params[:id]
   end
 
   def refresh_location
-    @stops = Stop.location(params[:location_id]).line(cookies[:line_filter].split('')).where(params[:day] => 't')
+    @stops = Stop.location(params[:location_id]).line(cookies[:line_filter].split('')).where(params[:day].downcase => 't')
     render :partial => 'location_stops'
   end
 end
